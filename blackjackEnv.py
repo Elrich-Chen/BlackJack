3ad7899dcf88a67c7179cd1de5f4440c79f2aa82
Elrich Chen <elrichchen123@gmail.com>
2025-07-08 12:43:48 -0400

    Refactoring code for readability and fixing bugs

--- blackjackEnv.py
+++ blackjackEnv.py
@@ -1,16 +1,19 @@
 import random
 
 class BlackjackEnv:
+    BLACKJACK = [10, "A"]
+    ALLOWED_SPLITS = 4
     def __init__(self):
         self.deck = []
         self.player_hand = []
         self.dealer_hand = []
         self.done = False  # Track if game is over
-        self.reset()       # Start new round
         self.split_active = False
         self.player_hands = []
         self.current_hand_index = 0
-        BLACKJACK = [10, "A"]
+        self.splits_number = 0
+
+        self.reset()       # Start new round
     
     def reset(self):
         """Starts a new round: shuffle deck, deal 2 cards each."""
@@ -78,35 +81,89 @@ class BlackjackEnv:
 
         return [score, is_soft, dealer_value] + percentages
     
-    
-    def is_bust(self, hand):
+    def check_bust(self, hand):
         return self.calculate_hand_value(hand) > 21
-    
-    def handle_split_transition(self):
-        self.done = False
-        self.current_hand_index = 1
-        self.player_hands[0] = self.player_hand
-        self.player_hand = self.player_hands[1]
-        return self.get_state(), 0, False, {"Now playing the second hand"}
-
-    """Trying to encapsualte all the helper functions needed in step"""
-    def player_hits(self):
+    def add_card(self):
         self.player_hand.append(self.deck.pop())
-        if self.is_bust(self.player_hand):
-            self.done = True
-            if (self.split_active and self.current_hand_index == 0):
-                return self.handle_split_transition()
-            
-            elif self.current_hand_index == 1:
-                return self.compare_split_hands()
 
-            return self.get_state(), -1, True, {"Player has Lost"}
+    """ACTION 0: PLAYER CHOOSES TO HIT"""
+    def handle_player_hit(self):
+        self.add_card()
+        self.done = self.check_bust(self.player_hand)
+        if self.done:
+            if (self.split_active):
+                return self.handle_split_transition()
+            return self.get_state(), -1, True, {"Player has Lost/Busted"}
         else:
-            return self.get_state(), 0, False, {}
+            return self.get_state(), 0, False, {"Player has hit, and can still continue !"}
     
+    """DEFAULT ACTION: DEALER HITS"""
     def dealer_hits(self):
         while self.calculate_hand_value(self.dealer_hand) < 17:
             self.dealer_hand.append(self.deck.pop())
+
+    """ACTION 1: PLAYER CHOOSES TO STAND"""
+    def handle_player_stand(self):
+        if (self.split_active):
+            return self.handle_split_transition()
+        self.dealer_hits()
+        return self.compare_hands()
+    
+    """ACTION 2: PLAYER CHOOSES TO DOUBLE"""
+    def handle_player_double(self):
+        if len(self.player_hand) != 2:
+            self.done = True
+            return self.get_state(), -1, True, {"Player does not have two cards"}  # Dealer wins
+        else:
+            self.add_card()
+
+            if self.split_active:
+                self.handle_split_transition()
+            
+            if self.check_bust(self.player_hand):
+                self.done = True
+                return self.get_state(), -1, True, {"Player has Lost/Busted"}
+            
+            self.dealer_hits()
+            return self.compare_hands()
+            
+    """ACTION 3: PLAYER CHOOSES TO SPLIT"""
+    def handle_player_split(self):
+        if (
+            len(self.player_hands) == 8 or 
+            len(self.player_hand) != 2 or 
+            self.player_hand[0] != self.player_hand[1] or
+            self.splits_number == self.ALLOWED_SPLITS
+        ):
+            self.done=True
+            return self.get_state(), -1, True, {}  # Dealer wins
+        
+        self.split_active = True
+        self.splits_number += 1
+        new_hand_1 = [self.player_hand[0], self.deck.pop()]
+        new_hand_2 = [self.player_hand[1], self.deck.pop()]
+        
+        try:
+            self.player_hands.pop(self.current_hand_index)
+        except IndexError:
+            pass
+        finally:
+            self.player_hands.insert(self.current_hand_index, new_hand_1)
+            self.player_hands.insert(self.current_hand_index+1, new_hand_2)
+
+        self.player_hand = self.player_hands[0]
+        return self.get_state(), 0, False, {"Split Started"}
+        
+    def advance_to_next_split_hand(self):
+        self.done = False
+        self.current_hand_index += 1
+        self.player_hand = self.player_hands[self.current_hand_index]
+        return self.get_state(), 0, False, {"Now playing the next hand"}
+
+    def handle_split_transition(self):
+        if (self.current_hand_index <len(self.player_hands)-1):
+            return self.advance_to_next_split_hand()
+        return self.compare_split_hands()
 
     def compare_hands(self):
         player_total = self.calculate_hand_value(self.player_hand)
@@ -114,107 +171,61 @@ class BlackjackEnv:
 
         self.done = True  # Game is over now
 
+        if set(self.player_hand) == set(self.BLACKJACK) and set(self.dealer_hand) != set(self.BLACKJACK) and len(self.player_hand)==2:
+            return self.get_state(), +1.5, True, {"BlackJack Win"}  # Player wins
+        elif set(self.player_hand) != set(self.BLACKJACK) and set(self.dealer_hand) == set(self.BLACKJACK) and len(self.dealer_hand)==2:
+            return self.get_state(), -1.5, True, {"BlackJack Loss"}  # DEALER wins
+        elif set(self.player_hand) == set(self.BLACKJACK) and set(self.dealer_hand) == set(self.BLACKJACK) and len(self.player_hand)==2 and len(self.dealer_hand)==2:
+            return self.get_state(), 0, True, {"BlackJack Tie"}  # TIE
+        
         if dealer_total > 21:
-            return self.get_state(), +1, True, {}  # Dealer busts → player wins
+            return self.get_state(), +1, True, {"Dealer Bust"}  # Dealer busts → player wins
         elif player_total > dealer_total:
-            return self.get_state(), +1, True, {}  # Player wins
+            return self.get_state(), +1, True, {"Player won, has a higher score"}  # Player wins
         elif player_total < dealer_total:
-            return self.get_state(), -1, True, {}  # Dealer wins
+            return self.get_state(), -1, True, {"Dealer won, has a higher score"}  # Dealer wins
         else:
-            return self.get_state(), 0, True, {}   # Tie (push)
+            return self.get_state(), 0, True, {"Tie, push game"}   # Tie (push)
 
     def compare_split_hands(self):
-        if (self.split_active and self.current_hand_index == 1):
-                self.dealer_hits()
-                
-                score = 0
-                self.player_hands[1] = self.player_hand
+        if (self.split_active and self.current_hand_index > 0):
+            self.dealer_hits()
+            
+            score = 0
 
-                for hand in self.player_hands:
-                    if self.is_bust(hand):
-                        score += -1
-                    else:
-                        self.player_hand = hand
-                        _, temp, _ , _ = self.compare_hands()
-                        score += temp
-                score = (score/2)
-                self.split_active = False
-                self.done = True
-                return self.get_state(), score, True, {"Split hand done"}
+            for hand in self.player_hands:
+                if self.is_bust(hand):
+                    score += -1
+                else:
+                    self.player_hand = hand
+                    _, temp, _ , _ = self.compare_hands()
+                    score += temp
+            score /= len(self.player_hands)
+            self.split_active = False
+            self.done = True
+            return self.get_state(), score, True, {"Split hand done"}
         else:
             return self.get_state(), -1, False, {"Split hand done"}
         
     def step(self, action):
-        """ 
-        Here our AI robot can now 'play' with the dealer by using the function {step()} 
-        If the AI chooses '0' : they are hitting
-        If the AI chooses '1': they are standing
-            Once they stand, the dealer hits until they reach a minimum score of 17.
-        The score is also evaluated in this self.comapre_hands function
-
-        also provides error checking for:
-        in case the function is called by our AI model, but game has been completed
-        """
-
         if self.done:
-            return self.get_state(), 0, True, {}
-            
+            return self.get_state(), 0, True, {"Game is finished !"}
+        
         if action == 0:
-            #player has chosen to hit
-            return self.player_hits()
-        
+            return self.handle_player_hit()
         elif action == 1:
-            #player has chosen to stand
-
-            #split game
-            if (self.split_active and self.current_hand_index == 0):
-                return self.handle_split_transition()
-            
-            if (self.split_active and self.current_hand_index == 1):
-                return self.compare_split_hands()
-
-            #return the state and who has won
-            self.dealer_hits()
-            return self.compare_hands()
-        
+            return self.handle_player_stand()
         elif action == 2:
-            #player has chosen to double
-            if len(self.player_hand) != 2:
-                self.done = True
-                return self.get_state(), -1, True, {}  # Dealer wins
-            else:
-                self.player_hand.append(self.deck.pop())
-                if (self.split_active and self.current_hand_index == 0):
-                        return self.handle_split_transition()
-                
-                elif (self.split_active and self.current_hand_index == 1):
-                    return self.compare_split_hands()
-                
-                else:
-                    if self.is_bust(self.player_hand):
-                        self.done = True
-                        return self.get_state(), -1, True, {"busted"}
-                    self.dealer_hits()
-                    return self.compare_hands()
-                
-        
+            return self.handle_player_double()
         elif action == 3:
-            #player has chosen to split
-            if len(self.player_hand) != 2 or self.player_hand[0] != self.player_hand[1]:
-                self.done=True
-                return self.get_state(), -1, True, {}  # Dealer wins
-            self.split_active = True
-            self.player_hands = [[self.player_hand[0], self.deck.pop()], [self.player_hand[1], self.deck.pop()]]
-            self.player_hand = self.player_hands[0]
-            return self.get_state(), 0, False, {"Split Started"}
+            return self.handle_player_split()
+        else:
+            return self.get_state(), -1, True, {"Invalid Game Option Chosen !"}
 
 
 
 if __name__ == "__main__":
     trial_game = BlackjackEnv()
-    while trial_game.player_hand[0] != trial_game.player_hand[1]:
-            trial_game.reset()
-
 
     while not trial_game.done:
         print(f"Player Hand : {trial_game.player_hand}")
